a75f09c2d28be1265ed14c0ee3af70f28d50b426
Jesse Cravens <jesse.cravens@gmail.com>
2012-04-02 21:22:50 -0500

    Changed title to remove techx

--- app.js
+++ app.js
@@ -60,5 +60,5 @@ app.get('/harmony.html', function(req, res){
 });
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3333);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
